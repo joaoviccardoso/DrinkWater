086c18dd065ab3647f95a7d710e5bd6aeaf85097
seu nome de usuário <pealq63@gmail.com>
2025-08-08 16:51:30 -0300

    Adicionada funcionalidade para o usuário definir sua meta diária de consumo de água e salvar no localStorage

--- src/componentes/barra/index.jsx
+++ src/componentes/barra/index.jsx
@@ -1,17 +1,41 @@
 import './barra.css'
+import Botao from '../botao'
+import { useState } from 'react';
 
 function Barra({contador}){
-    const meta = 2000;
+    const [inputValor, setInput] = useState('')
+    const [meta, setMeta] = useState(()=>{
+        const valorMeta = localStorage.getItem('meta')
+        return valorMeta ? parseFloat(valorMeta) : 0;
+    })
     const porcentagem = (contador / meta) * 100;
+
+    function definirMeta(){
+        setMeta(inputValor)
+        localStorage.setItem('meta', inputValor)
+    }
 
     return(
         <div className='container-barra'>
-            <p>{contador}ml de {meta}</p>
+            <p>{contador}ml de {meta}ml</p>
             <div className='barra'>
                 <div 
                     className='preenchimento'
                     style={{width: `${porcentagem}%`}}
                 ></div>
+            </div>
+            <div className='container-meta'>
+                <h3>Definir Meta</h3>
+                <input 
+                    type="number" 
+                    className='definirMeta'
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <Botao
+                    className={'salvarMeta'}
+                    texto="Salvar Meta"
+                    onClick={() => definirMeta()}
+                />
             </div>
         </div>
     )
